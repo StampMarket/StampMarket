52c5ee23ed96c361bf0a65b5eef9e1ea58750c93
Mask <shijiaxu.mask@qq.com>
2023-12-11 12:56:45 +0800

    添加注册和登录的异常处理逻辑

--- stampmarket-user/src/main/java/com/stampmarket/user/service/impl/UserServiceImpl.java
+++ stampmarket-user/src/main/java/com/stampmarket/user/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.stampmarket.user.service.impl;
 
 import com.stampmarket.common.utils.JwtTool;
 import com.stampmarket.user.dao.UserDao;
+import com.stampmarket.user.exception.CustomException;
 import com.stampmarket.user.pojo.User;
 import com.stampmarket.user.pojo.UserLoginDTO;
 import com.stampmarket.user.pojo.UserLoginVO;
@@ -18,11 +19,20 @@ public class UserServiceImpl implements UserService {
     private final UserDao userDao;
 
     public void register(User user) {
+        if(userDao.getByUsername(user.getUsername()) != null) {
+            throw new CustomException("用户名已存在");
+        }
         userDao.save(user);
     }
 
     public UserLoginVO login(UserLoginDTO userLoginDTO) {
         User user = userDao.getByUsername(userLoginDTO.getUsername());
+        if(user == null) {
+            throw new CustomException("用户名不存在");
+        }
+        if(!user.getPassword().equals(userLoginDTO.getPassword())) {
+            throw new CustomException("密码错误");
+        }
         Map<String, Object> claims = new HashMap<>();
         claims.put("id", user.getId());
         String token = JwtTool.getToken("stamp_market", 3600_000, claims);
