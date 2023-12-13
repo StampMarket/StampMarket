fd49ce70d07bf0e1863707f6fca975ca0b0d71a1
Mask <shijiaxu.mask@qq.com>
2023-12-13 14:50:47 +0800

    修改邮票价格单位为分

--- mysql-database/init.sql
+++ mysql-database/init.sql
@@ -18,10 +18,10 @@ create table stamp(
     width int unsigned comment '宽度，单位毫米',
     height int unsigned comment '高度，单位毫米',
     publish_date date comment '发行日期',
-    price double comment '单价'
+    price int comment '单价'
 );
 
 INSERT INTO stamp (name, description, width, height, publish_date, price) VALUES
-('蓝色花卉', '美丽的花卉邮票', 30, 40, '2023-01-15', 2.5),
-('历史人物', '重要历史人物邮票', 25, 35, '2023-02-20', 3.0),
-('动物世界', '精彩的动物邮票', 35, 45, '2023-03-10', 2.8);
+('蓝色花卉', '美丽的花卉邮票', 30, 40, '2023-01-15', 250),
+('历史人物', '重要历史人物邮票', 25, 35, '2023-02-20', 300),
+('动物世界', '精彩的动物邮票', 35, 45, '2023-03-10', 280);
