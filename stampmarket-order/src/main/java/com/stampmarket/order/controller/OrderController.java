f416f410e11b9ad4829f9fafad65613c6f018dba
Gorun <307532500@qq.com>
2023-12-13 12:21:35 +0800

    添加Order方法访问器

--- stampmarket-order/src/main/java/com/stampmarket/order/controller/OrderController.java
+++ stampmarket-order/src/main/java/com/stampmarket/order/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.stampmarket.order.controller;
 
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+    @PutMapping("/")
+    public String order() {
+        return "OK";
+    }
 }
