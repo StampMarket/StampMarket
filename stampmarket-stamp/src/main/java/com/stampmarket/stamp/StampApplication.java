9f8a65b16412eb997633c230a6628155349b02f9
qjhqj <123883665+qjhqj@users.noreply.github.com>
2023-12-14 20:30:52 +0800

    启动eureka注册功能

--- stampmarket-stamp/src/main/java/com/stampmarket/stamp/StampApplication.java
+++ stampmarket-stamp/src/main/java/com/stampmarket/stamp/StampApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StampApplication {
     public static void main(String[] args) {
         SpringApplication.run(StampApplication.class, args);
