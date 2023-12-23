8fbe8042311881069f7c96e576acb3b94220fc54
Mask <shijiaxu.mask@qq.com>
2023-12-23 09:34:53 +0800

    修改Result的code字段定义

--- stampmarket-common/src/main/java/com/stampmarket/common/pojo/Result.java
+++ stampmarket-common/src/main/java/com/stampmarket/common/pojo/Result.java
@@ -20,22 +20,14 @@ public class Result<T> implements Serializable {
     }
 
     public static <T> Result<T> success() {
-        return new Result<>(200, "OK", null);
-    }
-
-    public static <T> Result<T> success(String msg){
-        return new Result<>(200, msg, null);
+        return new Result<>(1, "OK", null);
     }
 
     public static <T> Result<T> success(T data) {
-        return new Result<>(200, "OK", data);
+        return new Result<>(1, "OK", data);
     }
 
     public static <T> Result<T> error(String msg) {
-        return new Result<>(500, msg, null);
-    }
-
-    public static <T> Result<T> error(int code, String msg) {
-        return new Result<>(code, msg, null);
+        return new Result<>(0, msg, null);
     }
 }
