07ddda810582f9bc1be6ebc2a9322f38603f4dde
Mask <shijiaxu.mask@qq.com>
2023-12-23 15:53:06 +0800

    去除测试代码

--- stampmarket-user/src/main/java/com/stampmarket/user/service/impl/UserServiceImpl.java
+++ stampmarket-user/src/main/java/com/stampmarket/user/service/impl/UserServiceImpl.java
@@ -35,8 +35,7 @@ public class UserServiceImpl implements UserService {
         }
         Map<String, Object> claims = new HashMap<>();
         claims.put("id", user.getId());
-//        String token = JwtTool.getToken("stamp_market", 3600_000, claims);
-        String token = JwtTool.getToken("stamp_market", 10, claims);
+        String token = JwtTool.getToken("stamp_market", 3600_000, claims);
         return new UserLoginVO(user.getId(), user.getUsername(), token, user.getBalance());
     }
 
