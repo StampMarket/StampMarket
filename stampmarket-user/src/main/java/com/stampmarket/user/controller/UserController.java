f43cf4a3f49b1822aab64b41d996703ba207004b
Gorun <307532500@qq.com>
2023-12-24 22:49:45 +0800

    添加UserController的CrossOrigin

--- stampmarket-user/src/main/java/com/stampmarket/user/controller/UserController.java
+++ stampmarket-user/src/main/java/com/stampmarket/user/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 @Slf4j
