c69d886b8b286213c08e5b3632a494b3994ffe03
hugo <hugoho912@outlook.com>
2023-12-28 17:50:40 +0800

    修正重复跨域问题

--- stampmarket-gateway/src/main/java/com/stampmarket/gateway/cors/CorsConfig.java
+++ stampmarket-gateway/src/main/java/com/stampmarket/gateway/cors/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.stampmarket.stamp.cors;
+package com.stampmarket.gateway.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,23 +6,16 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
-        // 跨域配置源
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        //设置跨域的配置信息
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        //1,允许任何来源
-        corsConfiguration.addAllowedOrigin("*");
-        //2,允许任何请求头
+        corsConfiguration.addAllowedOrigin("http://localhost:5173");
         corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
-        //3,允许任何方法
         corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
-        //4,允许凭证
         corsConfiguration.setAllowCredentials(true);
         source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(source);
