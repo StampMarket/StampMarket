bb2b6ff15dd61562a4265cccf77eb4c50cc7a18e
Gorun <307532500@qq.com>
2023-12-28 15:44:53 +0800

    添加hystric基本框架

--- stampmarket-order/src/main/java/com/stampmarket/order/client/StampController.java
+++ stampmarket-order/src/main/java/com/stampmarket/order/client/StampController.java
@@ -1,13 +1,12 @@
 package com.stampmarket.order.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "stamp")
-public interface StampClient {
-    @RequestMapping(value = "/stamp/price/{id}", method = RequestMethod.GET)
+@FeignClient(value = "stamp", fallback = StampControllerImpl.class)
+public interface StampController {
+    @RequestMapping(value = "/price/{id}", method = RequestMethod.GET)
     Integer getPriceForStampId(@PathVariable(value = "id") Integer id);
 }
